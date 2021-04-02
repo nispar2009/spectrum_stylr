362f6f472537f309870d2ce648c22e6b9deae0fa
nispar2009 <varun.parupalli@gmail.com>
2021-04-02 16:06:38 +0530

    SpectrumStylr JS file

---
+++ Spectrum.js
@@ -0,0 +1,8 @@
+function ShowContent() {
+    document.getElementById("nav-mobile-content").classList.toggle("n-m-show");
+}
+
+function HideContent() {
+
+    document.getElementById("nav-mobile-content").style.display = "none";
+}
