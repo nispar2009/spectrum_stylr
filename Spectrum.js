570e14cdace908d51fffd3297b291581be778dd8
nispar2009 <varun.parupalli@gmail.com>
2021-04-26 15:31:37 +0530

    Update23

--- Spectrum.js
+++ Spectrum.js
@@ -2,5 +2,5 @@ function ShowContent() {
     document.getElementById("nav-mobile-content").classList.toggle("n-m-show");
 }
 function redirect(newLocation) {
-    location.replace(newLocation);
+    window.location.replace(newLocation);
 }
