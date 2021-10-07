00b51d4d8c06d8a3fda65cd22eef16e872ab9414
nispar2009 <varun.parupalli@gmail.com>
2021-10-07 15:10:25 +0530

    MajorUpdate2

--- Spectrum.js
+++ Spectrum.js
@@ -1,18 +1,8 @@
-function ShowContent() {
-    document.getElementById("nav-mobile-content").classList.toggle("n-m-show");
-}
-function redirect(newLocation) {
-    window.location.replace(newLocation);
-}
-function setValue(id, val) {
-    const elem = document.getElementById(id)
-    elem.innerHTML = val
-}
-function getInput(id) {
-    const inp = document.getElementById(id).value
-    return (inp)
-}
-function filterList(arr, val) {
+ShowContent = () => document.getElementById("nav-mobile-content").classList.toggle("n-m-show")
+redirect = newLocation => window.location.replace(newLocation);
+setValue = (id, val) => document.getElementById(id).innerHTML = val
+getInput = id => document.getElementById(id).value
+filterList = (arr, val) =>  {
     let newArr = []
     for (const item of arr) {
         if (item.toLowerCase().includes(val.toLowerCase())) {
